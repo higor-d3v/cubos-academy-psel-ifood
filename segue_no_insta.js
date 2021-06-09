8f228bbc1164d2c78d453d7984229006a2e9696e
higor <hgrprofissional@gmail.com>
2021-06-08 23:19:51 -0300

    Me Segue No Insta!

---
+++ segue_no_insta.js
@@ -0,0 +1,11 @@
+function solucao(largura, altura) {
+    //seu codigo aqui
+    
+          if (largura>altura){
+              console.log("PAISAGEM")
+          }else if(largura===altura){
+              console.log("QUADRADA")     
+         }else{
+             console.log("RETRATO")
+         }
+  }
